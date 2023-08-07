3ee63cefa2444716d2897c82e8b25f34449df916
RidhoPutraBramantyo <126537811+RidhoPutraBramantyo@users.noreply.github.com>
2023-08-07 17:11:39 +0800

    update javascript

--- game.js
+++ game.js
@@ -1,6 +1,13 @@
+function getRandomNumber() {
+  return Math.floor(Math.random() * 3) + 1;
+}
+
 function startGame() {
   alert("Selamat datang di Game Tebak Angka!");
   alert("Setiap pemain harus menebak angka dari 1 hingga 3.");
+
+  let player1Score = 0;
+  let player2Score = 0;
 
   while (true) {
     let player1Guess = prompt("Player 1, tebak angka (1-3):");
@@ -19,20 +26,29 @@ function startGame() {
       continue;
     }
 
-    let targetNumber = Math.floor(Math.random() * 3) + 1;
+    let targetNumber = getRandomNumber();
     alert("Angka yang harus ditebak: " + targetNumber);
 
     if (player1Guess === targetNumber) {
       alert("Player 1 benar!");
+      player1Score++;
     } else {
       alert("Player 1 salah!");
     }
 
     if (player2Guess === targetNumber) {
       alert("Player 2 benar!");
+      player2Score++;
     } else {
       alert("Player 2 salah!");
     }
+
+    alert(
+      "Skor saat ini:<br>Player 1: " +
+        player1Score +
+        "<br>Player 2: " +
+        player2Score
+    );
 
     let playAgain = confirm("Apakah ingin bermain lagi?");
     if (!playAgain) {
