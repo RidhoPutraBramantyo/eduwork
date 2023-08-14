2de34ec69f7ce80cd6954f3defd5fa3783af5188
RidhoPutraBramantyo <126537811+RidhoPutraBramantyo@users.noreply.github.com>
2023-08-14 21:09:59 +0800

    update tugas conditonal variable

--- Javascript-Assigment/conditionalVariable.js
+++ Javascript-Assigment/conditionalVariable.js
@@ -19,3 +19,42 @@ const dataSiswa = [
 const listJudul = dataSiswa.map((buku) => buku.nama);
 
 console.log(listJudul);
+
+//==================================================
+
+// Latihan: Sistem Pengelolaan Pengguna
+const users = [
+  {
+    id: 1,
+    nama: "John Doe",
+    umur: 30,
+    saldo: 1000,
+  },
+  {
+    id: 2,
+    nama: "Jane Smith",
+    umur: 25,
+    saldo: 1500,
+  },
+  {
+    id: 3,
+    nama: "Michael Johnson",
+    umur: 28,
+    saldo: 2000,
+  },
+  {
+    id: 4,
+    nama: "Emily Brown",
+    umur: 22,
+    saldo: 800,
+  },
+];
+
+const targetId = 2;
+
+const user = users.find((user) => user.id === targetId);
+
+if (user) {
+  console.log(`Username: ${user.nama}`);
+  console.log(`Saldo: ${user.saldo}`);
+}
